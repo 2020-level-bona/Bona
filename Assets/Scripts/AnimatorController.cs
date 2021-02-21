313bcd276ac70b2be0ade1e12c6f2a327b7a9407
aren227 <mathdong01@gmail.com>
2021-02-21 18:00:51 +0900

    보나 애니메이션 구현

---
+++ Assets/Scripts/AnimatorController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimatorController : MonoBehaviour
+{
+    public Animator animator;
+    SpriteRenderer spriteRenderer;
+
+    bool flipped = false;
+
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    public void Play(AnimatorState state)
+    {
+        if (state.ShouldFlip)
+            spriteRenderer.flipX = state.FlipValue;
+        
+        animator.Play(state.Hash);
+    }
+}
