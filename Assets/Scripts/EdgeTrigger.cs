59c00f7ac178e8e30a968bb1a7a021210bc6d1ce
aren227 <mathdong01@gmail.com>
2021-02-21 12:44:07 +0900

    씬 22개 추가

---
+++ Assets/Scripts/EdgeTrigger.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EdgeTrigger : MonoBehaviour
+{
+    Player player;
+
+    EdgeCollider2D edgeCollider;
+    bool activated = false;
+
+    void Start()
+    {
+        player = FindObjectOfType<Player>();
+        edgeCollider = GetComponent<EdgeCollider2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 playerPosition = player.transform.position;
+        Vector2 closest = edgeCollider.ClosestPoint(playerPosition);
+        if (Vector2.Distance(playerPosition, closest) <= 0.25f) {
+            if (!activated) {
+                activated = true;
+                Activate();
+            }
+        }
+        else {
+            if (activated) {
+                activated = false;
+                Deactivate();
+            }
+        }
+    }
+
+    void Activate() {
+        if (player.currentFloor == 1) {
+            player.currentFloor = 2;
+        } else {
+            player.currentFloor = 1;
+        }
+        Debug.Log("Change Floor to " + player.currentFloor);
+    }
+
+    void Deactivate() {
+
+    }
+}
---
+++ Assets/Scripts/ZIndex.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ZIndex : MonoBehaviour
+{
+    [HideInInspector]
+    public float z = 0f;
+
+    public float y = 0f;
+    public float angle = 0f;
+
+    public int alwaysBehindFloor = -1;
+    public int alwaysFrontFloor = -1;
+
+    void Awake() {
+        z = transform.position.z;
+    }
+
+    void OnDrawGizmos() {
+        Gizmos.color = Color.red;
+        Gizmos.DrawLine(new Vector3(-10, y - 10 * Mathf.Tan(angle * Mathf.Deg2Rad), 0), new Vector3(10, y + 10 * Mathf.Tan(angle * Mathf.Deg2Rad), 0));
+    }
+
+    public bool IsBehindOf(Vector2 position) {
+        return y + Mathf.Tan(angle * Mathf.Deg2Rad) * position.x > position.y;
+    }
+
+}
