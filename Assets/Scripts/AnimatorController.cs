e29ce587496306fa66d829c2fa59ddd7d90dbac4
aren227 <mathdong01@gmail.com>
2021-02-21 20:13:16 +0900

    몇몇 씬의 Composite 오브젝트의 잘못된 Z 좌표 수정

--- Assets/Scripts/AnimatorController.cs
+++ Assets/Scripts/AnimatorController.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class AnimatorController : MonoBehaviour
 {
-    public Animator animator;
+    Animator animator;
     SpriteRenderer spriteRenderer;
 
     void Start()
