fc697354a06752e3baea9d578e05b922ceab0706
SungWonHwang <swon0331@gmail.com>
2021-02-26 22:29:16 +0900

    210226
    characters_Add

---
+++ Assets/Scripts/UI/MainMenu.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenu : MonoBehaviour
+{
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+    public void OnClickNewGame(){
+        Debug.Log("시작하기");
+    }
+
+    public void OnClickQuit(){
+#if UNITY_EDITOR
+      UnityEditor.EditorApplication.isPlaying = false;
+#else
+      Application.Quit();
+#endif
+
+    }
+}
