828cbc45629d75ad8c8557c9c2ef2e2189c1ce63
FlightFly98 <briansj99@gmail.com>
2021-03-02 20:07:57 +0900

    월드 간 이동 구현(기본, 좌표x)

---
+++ Assets/Scripts/TransferMap.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TransferMap : MonoBehaviour
+{
+    public string transferMapName; // 이동할 맵의 이름
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D other) 
+    {
+        if(other.gameObject.name == "Sprite")
+        {
+            SceneManager.LoadScene(transferMapName);
+        }
+    }
+}
