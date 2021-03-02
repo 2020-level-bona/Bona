3e551c3723b7f28942b53e75897a141640119c6d
SungWonHwang <swon0331@gmail.com>
2021-03-03 01:18:53 +0900

    인벤토리 UI 생성
    화면 하단에 핫바 제작

---
+++ Assets/Scripts/UI/InventoryUI.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InventoryUI : MonoBehaviour
+{
+ public GameObject InventoryUIPanel;
+ bool activeInventory = false;
+
+ private void Start()
+ {
+     InventoryUIPanel.SetActive(activeInventory);
+ }
+
+ private void Update()
+ {
+     if(Input.GetKeyDown(KeyCode.I))
+     {
+         activeInventory = !activeInventory;
+         InventoryUIPanel.SetActive(activeInventory);
+     }
+ }
+}
