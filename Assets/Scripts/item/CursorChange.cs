3253c01d7c65534135ab4214f2bac864306c6707
SungWonHwang <swon0331@gmail.com>
2021-03-03 23:08:39 +0900

    아이템에 닿으면 커서 모양 변화
    마우스 아이콘 변화

---
+++ Assets/Scripts/item/CursorChange.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CursorChange : MonoBehaviour
+{
+
+    public Texture2D cursorArrow;
+    public Texture2D cursorSecond;
+
+
+    void Start()
+    {
+        Cursor.SetCursor(cursorArrow, Vector2.zero, CursorMode.ForceSoftware);
+    }
+
+    void OnMouseEnter()
+    {
+        Cursor.SetCursor(cursorSecond, Vector2.zero, CursorMode.ForceSoftware);
+    }
+
+    void OnMouseExit()
+    {
+        Cursor.SetCursor(cursorArrow, Vector2.zero, CursorMode.ForceSoftware);
+    }
+
+}
