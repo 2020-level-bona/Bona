ab29c0d735e09a4844301c31d7930c48461499c6
aren227 <mathdong01@gmail.com>
2021-03-06 01:10:07 +0900

    기존 코드 주석처리, Item, ItemDefinition, ItemFactory 추가

---
+++ Assets/Scripts/인벤토리/ItemDefinition.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "ItemDefinition")]
+public class ItemDefinition : ScriptableObject
+{
+    public ItemType itemType;
+
+    public Sprite sprite;
+
+    public bool stackable;
+}
