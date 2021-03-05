ac5cfcee74d6f95a7318d8e451a8cedb328db4c2
aren227 <mathdong01@gmail.com>
2021-03-05 10:56:04 +0900

    MouseClickTrigger 구현

---
+++ Assets/Scripts/MouseClickTrigger.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class MouseClickTrigger : Trigger
+{
+    Camera cam;
+    PolygonCollider2D coll;
+
+    void Awake() {
+        coll = GetComponent<PolygonCollider2D>();
+        cam = Camera.main;
+    }
+
+    void Update() {
+        if (Input.GetMouseButtonDown(0)) {
+            Vector2 worldPos = cam.ScreenToWorldPoint(Input.mousePosition);
+            if (coll.OverlapPoint(worldPos)) {
+                Event.Invoke();
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/PrintMessages.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Trigger))]
+public class PrintMessages : MonoBehaviour
+{
+    public string[] messages;
+
+    Player player;
+    ChatQueue chatQueue;
+    Trigger trigger;
+
+    void Awake() {
+        player = FindObjectOfType<Player>();
+        chatQueue = FindObjectOfType<ChatQueue>();
+
+        trigger = GetComponent<Trigger>();
+    }
+
+    void Start() {
+        trigger.AddListener(Chat);
+    }
+
+    void Chat() {
+        foreach (string message in messages) {
+            chatQueue.AddChat(new Chat(message, player.transform));
+        }
+    }
+}
