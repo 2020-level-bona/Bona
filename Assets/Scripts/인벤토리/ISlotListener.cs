5b9d5c3fffc66f8202bdbd5ab047a46f3019f43e
aren227 <mathdong01@gmail.com>
2021-03-07 01:08:27 +0900

    Slot 프리팹 수정, ISlotListener 추가

---
+++ Assets/Scripts/인벤토리/ISlotListener.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface ISlotListener
+{
+    void OnSlotClick(int index);
+}
--- Assets/Scripts/인벤토리/Slot.cs
+++ Assets/Scripts/인벤토리/Slot.cs
@@ -4,8 +4,25 @@ using UnityEngine;
 using UnityEngine.UI;
 
 public class Slot : MonoBehaviour
-{    
+{
+    public int index {get; private set;}
+
     public Text qtyText;
 
     public Image itemImage;
+
+    public Button slotButton;
+
+    ISlotListener listener;
+
+    public void Initialize(int index, ISlotListener listener) {
+        this.index = index;
+        this.listener = listener;
+
+        slotButton.onClick.AddListener(OnClick);
+    }
+
+    void OnClick() {
+        listener.OnSlotClick(index);
+    }
 }
