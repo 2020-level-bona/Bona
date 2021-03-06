0112f9cedab399b02d52fe5eb1434705d2596aae
aren227 <mathdong01@gmail.com>
2021-03-07 01:46:37 +0900

    InventoryUI, InventoryCanvas 프리팹 외 대거 수정

--- Assets/Scripts/UI/InventoryUI.cs
+++ Assets/Scripts/UI/InventoryUI.cs
@@ -2,22 +2,38 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class InventoryUI : MonoBehaviour
+public class InventoryUI : MonoBehaviour, ISlotListener
 {
- public GameObject InventoryUIPanel;
- bool activeInventory = false;
+    public GameObject InventoryUIPanel;
+    public GameObject slotPrefab;
+    public Transform slotParent;
 
- private void Start()
- {
-     InventoryUIPanel.SetActive(activeInventory);
- }
+    bool activeInventory = false;
+    
+    private void Start() {
+        Player player = FindObjectOfType<Player>();
+        Inventory inventory = player.inventory;
 
- private void Update()
- {
-     if(Input.GetKeyDown(KeyCode.I))
-     {
-         activeInventory = !activeInventory;
-         InventoryUIPanel.SetActive(activeInventory);
-     }
- }
+        for (int i = 0; i < Inventory.NUM_SLOTS; i++) {
+            GameObject gameObject = Instantiate(slotPrefab);
+            Slot slot = gameObject.GetComponent<Slot>();
+            slot.Initialize(i, this);
+
+            gameObject.transform.SetParent(slotParent);
+            gameObject.transform.localScale = Vector3.one; // WTF?
+        }
+
+        InventoryUIPanel.SetActive(activeInventory);
+    }
+    
+    private void Update() {
+        if(Input.GetKeyDown(KeyCode.I)) {
+            activeInventory = !activeInventory;
+            InventoryUIPanel.SetActive(activeInventory);
+        }
+    }
+
+    public void OnSlotClick(int index) {
+        // TODO
+    }
 }
