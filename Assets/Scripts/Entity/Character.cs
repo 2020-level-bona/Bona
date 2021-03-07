04cb64e576f71abafde859ec8cc1d9c74f7e885f
aren227 <mathdong01@gmail.com>
2021-03-08 02:13:20 +0900

    Movable, Character 클래스 추가

---
+++ Assets/Scripts/Entity/Character.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Character : Movable
+{
+    public const CharacterType type = CharacterType.UNKNOWN;
+
+    public float height = 2f;
+
+#if UNITY_EDITOR
+    void Update() {
+        Debug.DrawRay(transform.position, Vector2.up * height, Color.blue);
+    }
+#endif
+}
