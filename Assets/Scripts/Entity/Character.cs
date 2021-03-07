e030ee820b6a1037dace88a1c56ab31c3b02df08
aren227 <mathdong01@gmail.com>
2021-03-08 02:48:05 +0900

    Level 클래스로 몇가지 기능 이전

--- Assets/Scripts/Entity/Character.cs
+++ Assets/Scripts/Entity/Character.cs
@@ -8,9 +8,31 @@ public class Character : Movable
 
     public float height = 2f;
 
+    Level level;
+
+    public int currentFloor {get; private set;} = 1;
+
+    protected virtual void Awake() {
+        level = FindObjectOfType<Level>();
+    }
+
 #if UNITY_EDITOR
-    void Update() {
+    protected virtual void Update() {
         Debug.DrawRay(transform.position, Vector2.up * height, Color.blue);
     }
 #endif
+
+    public override void MoveTo(Vector2 position) {
+        int floor = level.GetFloor(position);
+        if (floor > 0)
+            currentFloor = floor;
+
+        if (currentFloor > 0) {
+            base.MoveTo(level.floorPolygons[currentFloor - 1].ClosestPoint(position));
+        }
+    }
+
+    public void MoveDelta(Vector2 positionDelta) {
+        MoveTo((Vector2) transform.position + positionDelta);
+    }
 }
---
+++ Assets/Scripts/Level.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Level : MonoBehaviour
+{
+    public List<PolygonCollider2D> floorPolygons {get; private set;}
+
+    void Awake() {
+        floorPolygons = new List<PolygonCollider2D>();
+
+        int floorIndex = 1;
+        while(true) {
+            GameObject gameObject = GameObject.Find("Floor" + floorIndex);
+            if (gameObject == null)
+                break;
+            
+            PolygonCollider2D polygon = gameObject.GetComponent<PolygonCollider2D>();
+            if (polygon == null)
+                break;
+            
+            floorPolygons.Add(polygon);
+
+            // Add Holes
+            for (int i = 0; i < gameObject.transform.childCount; i++) {
+                PolygonCollider2D hole = gameObject.transform.GetChild(i).GetComponent<PolygonCollider2D>();
+                if (hole != null) {
+                    polygon.pathCount++;
+                    polygon.SetPath(polygon.pathCount - 1, hole.GetPath(0));
+                }
+            }
+
+            floorIndex++;
+        }
+    }
+
+    public int GetFloor(Vector2 position) {
+        for (int i = 0; i < floorPolygons.Count; i++) {
+            if (floorPolygons[i].OverlapPoint(position))
+                return i + 1;
+        }
+        return 0;
+    }
+}
