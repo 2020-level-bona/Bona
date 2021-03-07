27b2f306f2ff445a7a45de8361da0231e7f722a8
aren227 <mathdong01@gmail.com>
2021-03-08 03:16:41 +0900

    MoveDelta 메서드를 Movable로 이동

--- Assets/Scripts/Entity/Movable.cs
+++ Assets/Scripts/Entity/Movable.cs
@@ -7,4 +7,8 @@ public class Movable : MonoBehaviour
     public virtual void MoveTo(Vector2 position) {
         transform.position = position;
     }
+
+    public void MoveDelta(Vector2 positionDelta) {
+        MoveTo((Vector2) transform.position + positionDelta);
+    }
 }
