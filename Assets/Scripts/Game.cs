931f3443c7c0c9c0ef309b6192e8520619560259
aren227 <mathdong01@gmail.com>
2021-03-08 00:42:33 +0900

    Game 클래스 생성

---
+++ Assets/Scripts/Game.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Game : MonoBehaviour
+{
+    public SceneContext sceneContext;
+
+    public void TransferScene(SceneReference sceneReference) {
+        sceneContext.LastScenePath = SceneManager.GetActiveScene().path;
+        SceneManager.LoadScene(sceneReference);
+    }
+
+    public Vector2 GetPlayerSpawnPoint(Vector2 defaultSpawnPoint) {
+        foreach (TransferMap transferMap in FindObjectsOfType<TransferMap>()) {
+            if (transferMap.targetScene != null && transferMap.targetScene.ScenePath == sceneContext.LastScenePath)
+                return transferMap.transform.position;
+        }
+        return defaultSpawnPoint;
+    }
+}
