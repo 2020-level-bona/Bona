d2ef69af597e89f986800899327fa4697f8ca473
SungWonHwang <swon0331@gmail.com>
2021-03-08 17:41:54 +0900

    메인메뉴 씬 연결 & 저장 기능 시도
    메인메뉴 씬 연결 & 저장 기능 시도

---
+++ Assets/Scripts/Scene/DialogueManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogueManager : MonoBehaviour
+{
+      static DialogueManager instance = null;
+      public static DialogueManager Instance()
+      
+      {
+          return instance;
+      }
+	  
+      void Awake () 
+      {
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(this.gameObject);
+        }
+        else 
+        {
+            if (this != instance)
+            {
+                Destroy(this.gameObject);
+            }
+        }
+    }
+}
