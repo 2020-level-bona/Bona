f0e99c9b20b9a7d521398079eb883443b7410238
aren227 <mathdong01@gmail.com>
2021-03-08 12:39:07 +0900

    ICameraOperator 추가

---
+++ Assets/Scripts/Camera/CameraOperatorType.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum CameraOperatorType
+{
+    REPLACE, // 카메라의 좌표는 해당 오퍼레이터의 좌표로 설정된다. 일반적인 카메라 움직임에 사용된다.
+    ADDITIVE // 카메라의 좌표는 이전 오퍼레이터까지 계산된 좌표에 덧셈하여 설정된다. 카메라 진동 표현에 사용될 수 있다.
+}
