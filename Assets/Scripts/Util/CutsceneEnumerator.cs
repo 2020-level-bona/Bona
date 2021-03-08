a4dd7fe8a932b51947db1ca8f8dafdcc57471279
aren227 <mathdong01@gmail.com>
2021-03-08 17:58:48 +0900

    StartCutscene 메서드 추가

---
+++ Assets/Scripts/Util/CutsceneEnumerator.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// 컷신 제어용 Wrapper 클래스
+public class CutsceneEnumerator : IEnumerator
+{
+    IEnumerator coroutine;
+
+    public object Current {
+        get => coroutine.Current;
+    }
+
+    public CutsceneEnumerator(IEnumerator coroutine) {
+        this.coroutine = coroutine;
+    }
+
+    public bool MoveNext() {
+        return coroutine.MoveNext();
+    }
+
+    public void Reset() {
+        coroutine.Reset();
+    }
+}
