cb6ab8f68ce7cf4ab347008f1d695d0e06f50643
aren227 <mathdong01@gmail.com>
2021-03-08 16:28:17 +0900

    Tween 기초 구현

---
+++ Assets/Scripts/Tween/TweenEntry.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TweenEntry<T> : ITweenEntry
+{
+    public UnityEngine.Object owner;
+    public Action<T> action;
+    public ILerpable<T> range;
+    public float start;
+    public float duration;
+
+    public TweenEntry(UnityEngine.Object owner, Action<T> action, ILerpable<T> range, float duration) {
+        this.owner = owner;
+        this.action = action;
+        this.range = range;
+        this.duration = duration;
+
+        start = Time.time;
+    }
+
+    public bool HasDone() {
+        if (!owner)
+            return true;
+        
+        if (start + duration <= Time.time) {
+            action(range.Lerp(1f));
+            return true;
+        }
+
+        return false;
+    }
+
+    public void Update() {
+        action(range.Lerp((Time.time - start) / duration));
+    }
+}
---
+++ Assets/Scripts/Util/Vector2Lerp.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Vector2Lerp : ILerpable<Vector2>
+{
+    public Vector2 from, to;
+
+    public Vector2Lerp(Vector2 from, Vector2 to) {
+        this.from = from;
+        this.to = to;
+    }
+
+    public Vector2 Lerp(float t) {
+        return Vector2.Lerp(from, to, t);
+    }
+}
