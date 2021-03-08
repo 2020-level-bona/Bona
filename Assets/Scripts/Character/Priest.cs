38c24de20321456838d633646cb7b10460f5c748
aren227 <mathdong01@gmail.com>
2021-03-08 17:59:23 +0900

    CutsceneTest 씬 및 사제 프리팹 수정

---
+++ Assets/Scripts/Character/Priest.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Priest : Character
+{
+    public override CharacterType type {
+        get => CharacterType.PRIEST;
+    }
+}
