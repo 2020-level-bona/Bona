2a49343c25a2bbb584086a34ad1d7854afef1a97
aren227 <mathdong01@gmail.com>
2021-03-08 18:42:13 +0900

    대화가 머리 위에 나타나도록 수정

--- Assets/Scripts/Scene/보나의집안.cs
+++ Assets/Scripts/Scene/보나의집안.cs
@@ -12,7 +12,7 @@ public class 보나의집안 : MonoBehaviour
         chatQueue = FindObjectOfType<ChatQueue>();
         player = FindObjectOfType<Player>();
 
-        chatQueue.AddChat(new Chat("좋은 아침! 오늘도 고달픈 하루가 시작되는구나.", player.transform));
-        chatQueue.AddChat(new Chat("배고파...", player.transform));
+        chatQueue.AddChat(new Chat("좋은 아침! 오늘도 고달픈 하루가 시작되는구나.", player));
+        chatQueue.AddChat(new Chat("배고파...", player));
     }
 }
--- Assets/Scripts/Test/ChatTest.cs
+++ Assets/Scripts/Test/ChatTest.cs
@@ -9,9 +9,9 @@ public class ChatTest : MonoBehaviour
     void Start() {
         chatQueue = FindObjectOfType<ChatQueue>();
 
-        chatQueue.AddChat(new Chat("안녕, 세계!", transform));
+        /*chatQueue.AddChat(new Chat("안녕, 세계!", transform));
         chatQueue.AddChat(new Chat("오늘도 좋은 하루!", transform));
         chatQueue.AddChat(new Chat("긴 문장을 말할 때에도 글자가 불안정하게 다음줄로 넘어가지 않아요. 이렇게띄어쓰기가없어도그럭저럭잘출력해줄겁니다아마도요.", transform));
-        chatQueue.AddChat(new Chat("솔직히 정말 맘에 안들긴 하네요.", transform));
+        chatQueue.AddChat(new Chat("솔직히 정말 맘에 안들긴 하네요.", transform));*/
     }
 }
