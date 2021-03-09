c2f158df643edd61ed66af90ff727881e05f7102
aren227 <mathdong01@gmail.com>
2021-03-09 12:12:42 +0900

    Path 구현

---
+++ Assets/Scripts/Util/Path/LinearPathSolver.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LinearPathSolver : IPathSolver
+{
+    public float GetNextT(Vector2[] points, float prevT, float moveLength) {
+        prevT = Mathf.Clamp(prevT, 0f, points.Length - 1);
+        moveLength = Mathf.Max(moveLength, 0f);
+
+        int index = Mathf.FloorToInt(prevT);
+        if (index == points.Length - 1)
+            return points.Length - 1;
+        
+        Vector2 A = points[index];
+        Vector2 B = points[index + 1];
+
+        float t = prevT + moveLength / (B - A).magnitude;
+        t = Mathf.Min(t, index + 1);
+
+        return t;
+    }
+
+    public float GetMaxT(Vector2[] points) {
+        return points.Length - 1;
+    }
+
+    public Vector2 GetPosition(Vector2[] points, float t) {
+        t = Mathf.Clamp(t, 0f, points.Length - 1);
+
+        int index = Mathf.FloorToInt(t);
+        if (index == points.Length - 1)
+            return points[points.Length - 1];
+        
+        Vector2 A = points[index];
+        Vector2 B = points[index + 1];
+
+        return Vector2.Lerp(A, B, t - index);
+    }
+}
---
+++ Assets/Scripts/Util/Path/Path.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Path : MonoBehaviour
+{
+    public PathType type = PathType.LINEAR;
+
+    void OnDrawGizmos() {
+        Vector2[] points = GetPoints();
+        IPathSolver solver = GetSolver(type);
+
+        Gizmos.color = Color.yellow;
+        for (int i = 0; i < points.Length; i++) {
+            Gizmos.DrawSphere(points[i], 0.2f);
+        }
+
+        Gizmos.color = Color.red;
+        for (float t = 0; t < solver.GetMaxT(points); t += 0.1f) {
+            Gizmos.DrawLine(solver.GetPosition(points, t), solver.GetPosition(points, t + 0.1f));
+        }
+    }
+
+    public Vector2[] GetPoints() {
+        Vector2[] paths = new Vector2[transform.childCount];
+        for (int i = 0; i < transform.childCount; i++) {
+            paths[i] = transform.GetChild(i).transform.position;
+        }
+        return paths;
+    }
+
+    public IPathSolver GetSolver(PathType type) {
+        switch (type) {
+            case PathType.LINEAR:
+                return new LinearPathSolver();
+            case PathType.BEZIER:
+                return new BezierPathSolver();
+        }
+        return null;
+    }
+}
