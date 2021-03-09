45b061017859407ca971fff1dc1070c336feb820
aren227 <mathdong01@gmail.com>
2021-03-09 15:57:46 +0900

    WaitForTween 추가

---
+++ Assets/Scripts/Util/WaitForTween.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// 이 클래스는 오로지 지시용으로만 사용된다.
+public class WaitForTween
+{
+    public ITweenEntry tweenEntry;
+
+    public WaitForTween(ITweenEntry tweenEntry) {
+        this.tweenEntry = tweenEntry;
+    }
+}
