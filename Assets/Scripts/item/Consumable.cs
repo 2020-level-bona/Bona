2459c09d31e40006fbab060b172630d6431f8d15
aren227 <mathdong01@gmail.com>
2021-03-18 15:26:36 +0900

    Consumable 스크립트 이동 및 작성

---
+++ Assets/Scripts/item/Consumable.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Consumable : MonoBehaviour
+{
+    public ItemType itemType;
+    public int quantity = 1;
+
+    Item item;
+
+    Player player;
+    Trigger trigger;
+
+    void Awake() {
+        player = FindObjectOfType<Player>();
+        trigger = GetComponent<Trigger>();
+    }
+
+    void Start() {
+        item = new Item(itemType, quantity);
+
+        if (trigger)
+            trigger.AddListener(Consume);
+        else
+            Debug.LogError("Trigger 컴포넌트가 없습니다. MouseClickTrigger 컴포넌트가 부착되어있는지 확인해주세요.");
+    }
+
+    void Consume() {
+        player.inventory.AddItem(item);
+        Destroy(gameObject);
+    }
+}
