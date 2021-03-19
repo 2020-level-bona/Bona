2f4bcdecc27a2253383213acb553bef55677ec56
SungWonHwang <swon0331@gmail.com>
2021-03-20 03:05:29 +0900

    #4-10p. 보나의집안 스크립트
    #4-10p. 보나의집안 스크립트

--- Assets/Scripts/Character/WaterFairy.cs
+++ Assets/Scripts/Character/WaterFairy.cs
@@ -7,4 +7,7 @@ public class WaterFairy : Character
     public override CharacterType type {
         get => CharacterType.WATERFAIRY;
     }
+
+    public static readonly AnimatorState STAND = new AnimatorState("스탠딩");
+    public static readonly AnimatorState WATER = new AnimatorState("물방울 만들며 장난치기");
 }
--- Assets/Scripts/Scene/보나의집안_4_10.cs
+++ Assets/Scripts/Scene/보나의집안_4_10.cs
@@ -41,35 +41,53 @@ public class 보나의집안_4_10 : MonoBehaviour
         Player bona = level.GetSpawnedCharacter(CharacterType.BONA) as Player;
         WaterFairy waterFairy = level.GetSpawnedCharacter(CharacterType.WATERFAIRY) as WaterFairy;
 
-        //tween으로 보나와 장난치는 요정 표현
-        /* ITweenEntry tweenEntry = Tween.Add(waterFairy, waterFairyPath.GetPath(), 7f);
-        yield return new WaitForTween(tweenEntry); */
+        //보나를 정해진 위치로 옯긴다.
+        ITweenEntry tweenEntry = Tween.Add(bona, bonaStandingPoint.position, 5f);
+        yield return new WaitForTween(tweenEntry);
+
+        //보나와 장난치는 요정 표현 (보나,요정 상호작용)
+        ITweenEntry tweenEntry2 = Tween.Add(waterFairy, waterFairyPath.GetPath(), 2f);
+        waterFairy.PlayAnimation(WaterFairy.WATER);
+
+        yield return new WaitForTween(tweenEntry2);
 
 
         // 모르 문에서 등장
         Moreu moreu = level.SpawnCharacter(CharacterType.MOREU, moreuComeInPoint.position) as Moreu;
 
+        waterFairy.PlayAnimation(WaterFairy.STAND);
+        yield return new WaitForSeconds(2f);
+        
+
         // 모르를 보나 앞으로 움직임
-        ITweenEntry tweenEntry2 = Tween.Add(moreu, moreuStandingPoint.position, 5f);
-        yield return new WaitForTween(tweenEntry2);
+        ITweenEntry tweenEntry3 = Tween.Add(moreu, moreuStandingPoint.position, 5f);
+        yield return new WaitForTween(tweenEntry3);
+        yield return new WaitForSeconds(2f);
 
         chatQueue.AddChat(new Chat("딱 걸렸어, 이 마녀!" , moreu));
         chatQueue.AddChat(new Chat("어쩐지 요즘 수상했어.", moreu));
         chatQueue.AddChat(new Chat("그 이상한 괴물로 우리를 다 죽일 셈이지?", moreu));
         chatQueue.AddChat(new Chat("전부 이를거야!", moreu));
+        yield return new WaitForSkippingChat();
+        yield return new WaitForSeconds(2f);
+        chatQueue.Next();
 
         //모르를 다시 문앞으로
-        ITweenEntry tweenEntry3 = Tween.Add(moreu, moreuOutPoint.position, 5f);
+        ITweenEntry tweenEntry4 = Tween.Add(moreu, moreuOutPoint.position, 5f);
+        yield return new WaitForTween(tweenEntry4);
 
         //모르 퇴장
         moreu.Hide();
 
-
         //요정과 보나 놀란다(!!띄우기)
+        bona.PlayAnimation(Player.SURPRISE);
+        chatQueue.AddChat(new Chat("!!!!!!" , bona));
+        chatQueue.AddChat(new Chat("!!!!", waterFairy));
+        yield return new WaitForSkippingChat();
+
 
     }
 
 
 
 }
-
