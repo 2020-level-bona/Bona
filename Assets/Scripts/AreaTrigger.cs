85fc4bc65516076da8780e0e13933ef944041217
aren227 <mathdong01@gmail.com>
2021-03-21 01:57:54 +0900

    AreaTrigger 구현

---
+++ Assets/Scripts/AreaTrigger.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Collider2D))]
+public class AreaTrigger : Trigger
+{
+    Game game;
+    Player player;
+    Collider2D coll;
+
+    void Awake() {
+        game = FindObjectOfType<Game>();
+        player = FindObjectOfType<Player>();
+        coll = GetComponent<Collider2D>();
+    }
+
+    void Update() {
+        if (coll.OverlapPoint(player.transform.position) && !game.IsPlayingCutscene) {
+            Event.Invoke();
+        }
+    }
+}
