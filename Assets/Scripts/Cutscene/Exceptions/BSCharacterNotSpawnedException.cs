254af298d4374588466b319804f5bbdecfbc64c8
aren227 <mathdong01@gmail.com>
2021-03-22 19:16:01 +0900

    여러가지 BSException 추가

---
+++ Assets/Scripts/Cutscene/Exceptions/BSCharacterNotSpawnedException.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class BSCharacterNotSpawnedException : BSException
+{
+    public BSCharacterNotSpawnedException() { }
+    public BSCharacterNotSpawnedException(string message) : base(message) { }
+    public BSCharacterNotSpawnedException(string message, System.Exception inner) : base(message, inner) { }
+    protected BSCharacterNotSpawnedException(
+        System.Runtime.Serialization.SerializationInfo info,
+        System.Runtime.Serialization.StreamingContext context) : base(info, context) { }
+}
