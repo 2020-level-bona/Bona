59e497ed980f3d3a9e040b7e8f2a99461ae1c06a
aren227 <mathdong01@gmail.com>
2021-03-22 19:18:31 +0900

    CommandLineParser에 더 다양한 타입 지원

--- Assets/Scripts/Cutscene/CommandLineParser.cs
+++ Assets/Scripts/Cutscene/CommandLineParser.cs
@@ -10,6 +10,10 @@ public class CommandLineParser : ICommandLineParser
         args = CommandLineTokenizer.Tokenize(line);
     }
 
+    void CheckIndex(int index) {
+        throw new BSSyntaxException($"{index}번째 파라미터가 필요하지만 전달되지 않았습니다.");
+    }
+
     public string GetKeyword() {
         return args[0];
     }
@@ -19,14 +23,53 @@ public class CommandLineParser : ICommandLineParser
     }
 
     public string GetString(int index) {
+        CheckIndex(index);
         return args[index];
     }
 
     public int GetInt(int index) {
-        return int.Parse(args[index]);
+        CheckIndex(index);
+        try {
+            return int.Parse(args[index]);
+        } catch (System.Exception) {
+            throw new BSSyntaxException($"{args[index]}를 정수 타입으로 변환할 수 없습니다.");
+        }
     }
 
     public float GetFloat(int index) {
-        return float.Parse(args[index]);
+        CheckIndex(index);
+        try {
+            return float.Parse(args[index]);
+        } catch (System.Exception) {
+            throw new BSSyntaxException($"{args[index]}를 실수 타입으로 변환할 수 없습니다.");
+        }
+    }
+
+    public CharacterType GetCharacterType(int index) {
+        string type = GetString(index);
+        try {
+            return (CharacterType) System.Enum.Parse(typeof(CharacterType), type);
+        } catch (System.Exception) {
+            throw new BSSyntaxException($"{args[index]}은(는) 올바른 캐릭터 타입명이 아닙니다.");
+        }
+    }
+
+    public Vector2Face GetVector2Face(Level level, int index) {
+        string str = GetString(index);
+        if (str.Contains(",")) {
+            try {
+                string[] spl = str.Split(',');
+                float x = float.Parse(spl[0]);
+                float y = float.Parse(spl[1]);
+                return new Vector2Face(new Vector2(x, y));
+            } catch (System.Exception) {
+                throw new BSSyntaxException($"{str}을(를) Vector2 타입으로 변환할 수 없습니다.");
+            }
+        } else {
+            Marker marker = level.GetMarker(str);
+            if (marker == null)
+                throw new BSMarkerNotFoundException($"마커[name={str}]를 찾을 수 없습니다.");
+            return marker;
+        }
     }
 }
