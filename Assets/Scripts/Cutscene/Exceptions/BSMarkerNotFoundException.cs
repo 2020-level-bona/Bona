53fd9e07f8a38df406e9d36d0def9de98e340e0c
aren227 <mathdong01@gmail.com>
2021-03-22 19:16:01 +0900

    여러가지 BSException 추가

---
+++ Assets/Scripts/Cutscene/Exceptions/BSMarkerNotFoundException.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class BSMarkerNotFoundException : BSException
+{
+    public BSMarkerNotFoundException() { }
+    public BSMarkerNotFoundException(string message) : base(message) { }
+    public BSMarkerNotFoundException(string message, System.Exception inner) : base(message, inner) { }
+    protected BSMarkerNotFoundException(
+        System.Runtime.Serialization.SerializationInfo info,
+        System.Runtime.Serialization.StreamingContext context) : base(info, context) { }
+}
---
+++ Assets/Scripts/Cutscene/Exceptions/BSMovableNotFoundException.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class BSMovableNotFoundException : BSException
+{
+    public BSMovableNotFoundException() { }
+    public BSMovableNotFoundException(string message) : base(message) { }
+    public BSMovableNotFoundException(string message, System.Exception inner) : base(message, inner) { }
+    protected BSMovableNotFoundException(
+        System.Runtime.Serialization.SerializationInfo info,
+        System.Runtime.Serialization.StreamingContext context) : base(info, context) { }
+}
