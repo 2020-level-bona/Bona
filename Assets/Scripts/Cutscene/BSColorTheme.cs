a0ef4ce7bce2ef5bed0864d96f53b19a6b53f3d8
aren227 <mathdong01@gmail.com>
2021-03-23 16:19:54 +0900

    BSColorTheme 구현

---
+++ Assets/Scripts/Cutscene/BSColorTheme.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName="BS Color Theme")]
+public class BScriptColorTheme : ScriptableObject
+{
+    static BScriptColorTheme instance;
+
+    public static BScriptColorTheme Instance {
+        get {
+            if (instance == null)
+                instance = Resources.Load("BScript/Default Color Theme") as BScriptColorTheme;
+            return instance;
+        }
+    }
+
+    public Color keywordColor;
+    public Color normalColor;
+    public Color nameColor;
+    public Color strColor;
+    public Color numColor;
+    public Color commentColor;
+}
