4357b3199e8f51b5414edfdb9e9f3d11533b59ad
aren227 <mathdong01@gmail.com>
2021-03-24 22:47:17 +0900

    And Or 연산자 추가 및 테스트 작성

---
+++ Assets/Scripts/Cutscene/Expression/Operators/GreaterEqualOp.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GreaterEqualOp : BinaryOp
+{
+    public int Priority => 3;
+
+    bool leftIsGreater;
+
+    public GreaterEqualOp(bool leftIsGreater) {
+        this.leftIsGreater = leftIsGreater;
+    }
+
+    public object Do(object a, object b) {
+        if (a is null)
+            a = 0;
+        if (b is null)
+            b = 0;
+        
+        if (a is bool)
+            throw new BSUnsupportedTypeException(-1, "왼쪽 피연산자의 타입 bool은 지원되지 않습니다.");
+        if (b is bool)
+            throw new BSUnsupportedTypeException(-1, "오른쪽 피연산자의 타입 bool은 지원되지 않습니다.");
+        
+        if (a is int && b is int)
+            return leftIsGreater ? ((int) a >= (int) b) : ((int) a <= (int) b);
+        
+        return leftIsGreater ? (System.Convert.ToSingle(a) >= System.Convert.ToSingle(b))
+            : (System.Convert.ToSingle(a) <= System.Convert.ToSingle(b));
+    }
+}
---
+++ Assets/Scripts/Cutscene/Expression/Operators/OrOp.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class OrOp : BinaryOp
+{
+    public int Priority => 0;
+
+    public object Do(object a, object b) {
+        if (a is null)
+            a = false;
+        
+        if (b is null)
+            b = false;
+        
+        if (!(a is bool))
+            throw new BSUnsupportedTypeException(-1, $"왼쪽 피연산자의 타입 {a.GetType()}은 지원되지 않습니다.");
+        if (!(b is bool))
+            throw new BSUnsupportedTypeException(-1, $"오른쪽 피연산자의 타입 {b.GetType()}은 지원되지 않습니다.");
+        
+        return (bool) a || (bool) b;
+    }
+}
