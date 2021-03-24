51c98c38203e4649783822e14875fc2eadfd9dcc
aren227 <mathdong01@gmail.com>
2021-03-25 03:01:55 +0900

    TriggerExecutionType 추가

---
+++ Assets/Scripts/Cutscene/Trigger/TriggerExecutionType.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum TriggerExecutionType
+{
+    [InspectorName("오직 한번만 실행")]
+    ONCE,
+    [InspectorName("여러번 실행")]
+    ANWAYS,
+}
