bcdb160e9d4aa35c0a5398a6e865b46166530693
aren227 <mathdong01@gmail.com>
2021-03-26 00:41:06 +0900

    Set 명령어 추가

---
+++ Assets/Scripts/BScript/Commands/SetCommand.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SetCommand : IScriptCommand
+{
+    string path;
+    string value;
+
+    public const string Keyword = "SET";
+    public bool Blocking => true;
+    public int LineNumber {get;}
+
+    public SetCommand(string path, string value) {
+        this.path = path;
+        this.value = value;
+    }
+
+    public SetCommand(CommandLineParser lineParser) {
+        LineNumber = lineParser.lineNumber;
+        this.path = lineParser.GetString(1);
+        this.value = lineParser.GetString(2);
+    }
+
+    public IEnumerator GetCoroutine() {
+        object evaluatedValue = Session.Instance.Get(path);
+        Session.Instance.Set(path, evaluatedValue);
+        yield return null; 
+    }
+}
