8af1eba8e6b1fbca2666e91dd0ff05499176634e
aren227 <mathdong01@gmail.com>
2021-03-25 18:56:06 +0900

    IAnimationController 추가

---
+++ Assets/Scripts/Animation/WalkAnimationController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Movable))]
+public class WalkAnimationController : MonoBehaviour, IAnimationController
+{
+    Movable movable;
+    public string idleClip;
+    public string leftClip, upClip, downClip;
+
+    void Awake() {
+        movable = GetComponent<Movable>();
+    }
+
+    public string GetClip() {
+        float x = movable.velocity.x;
+        float y = movable.velocity.y;
+
+        if (Mathf.Abs(x) <= 0.1f && Mathf.Abs(y) <= 0.1f) {
+            return idleClip;
+        } else if (Mathf.Abs(x) >= Mathf.Abs(y)) {
+            return leftClip;
+        } else {
+            if (y > 0) {
+                return upClip;
+            } else {
+                return downClip;
+            }
+        }
+    }
+}
