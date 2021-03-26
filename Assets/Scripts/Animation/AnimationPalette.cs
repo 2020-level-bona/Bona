442544b984bbeef446aa2a511486211925c9a782
aren227 <mathdong01@gmail.com>
2021-03-26 17:45:20 +0900

    AnimationPalette 구현

---
+++ Assets/Scripts/Animation/AnimationPalette.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Animator))]
+public class AnimationPalette : MonoBehaviour
+{
+    public List<AnimationClipNamePair> pairs = new List<AnimationClipNamePair>();
+
+    void OnValidate() {
+        List<string> clipNames = new List<string>();
+        foreach (AnimationClip clip in GetComponent<Animator>().runtimeAnimatorController.animationClips)
+            clipNames.Add(clip.name);
+        
+        pairs.RemoveAll(x => !clipNames.Contains(x.clipName));
+    }
+
+    public List<string> CheckConflicts() {
+        List<string> conflictedNames = new List<string>();
+        for (int i = 0; i < pairs.Count; i++) {
+            for (int j = i + 1; j < pairs.Count; j++) {
+                if (pairs[i].alias == pairs[j].alias && !conflictedNames.Contains(pairs[i].alias))
+                    conflictedNames.Add(pairs[i].alias);
+            }
+        }
+        return conflictedNames;
+    }
+
+    public string GetClipName(string alias) {
+        foreach (AnimationClipNamePair pair in pairs) {
+            if (pair.alias == alias)
+                return pair.clipName;
+        }
+        return null;
+    }
+
+    public string GetAlias(string clipName) {
+        foreach (AnimationClipNamePair pair in pairs) {
+            if (pair.clipName == clipName)
+                return pair.alias;
+        }
+        return null;
+    }
+
+    public void RemoveAlias(string clipName) {
+        pairs.RemoveAll(x => x.clipName == clipName);
+    }
+
+    public void SetAlias(string clipName, string alias) {
+        foreach (AnimationClipNamePair pair in pairs) {
+            if (pair.clipName == clipName) {
+                pair.alias = alias;
+                return;
+            }
+        }
+        pairs.Add(new AnimationClipNamePair(clipName, alias));
+    }
+}
+
+[System.Serializable]
+public class AnimationClipNamePair {
+    public string clipName;
+    public string alias;
+
+    public AnimationClipNamePair(string clipName, string alias) {
+        this.clipName = clipName;
+        this.alias = alias;
+    }
+}
---
+++ Assets/Scripts/Animation/AnimationPaletteEditor.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(AnimationPalette))]
+public class AnimationPaletteEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        AnimationPalette palette = (AnimationPalette) target;
+
+        Animator animator = palette.GetComponent<Animator>();
+        var clips = animator.runtimeAnimatorController.animationClips;
+
+        EditorGUILayout.LabelField($"총 {clips.Length}개의 애니메이션 클립이 로드되었습니다.");
+        
+        List<string> conflicts = palette.CheckConflicts();
+        if (conflicts.Count > 0) {
+            EditorGUILayout.LabelField($"{conflicts.Count}개의 이름 충돌이 발견되었습니다!", GetErrorTextStyle());
+            EditorGUILayout.LabelField(string.Join(", ", conflicts), GetErrorTextStyle());
+            EditorGUILayout.LabelField("부여된 별칭이 모두 고유하도록 재설정해주세요.", GetErrorTextStyle());
+        }
+
+        foreach (AnimationClip clip in clips) {
+            EditorGUILayout.BeginHorizontal();
+            EditorGUILayout.LabelField(clip.name);
+
+            string alias = EditorGUILayout.TextField(palette.GetAlias(clip.name));
+            if (alias == null || alias.Length == 0) {
+                if (palette.GetAlias(clip.name) != null)
+                    palette.RemoveAlias(clip.name);
+            } else {
+                palette.SetAlias(clip.name, alias);
+            }
+
+            EditorGUILayout.EndHorizontal();
+        }
+    }
+
+    GUIStyle GetErrorTextStyle() {
+        GUIStyle style = new GUIStyle();
+        style.normal.textColor = Color.red;
+        style.fontStyle = UnityEngine.FontStyle.Bold;
+        return style;
+    }
+}
