ebcaf91ad9c2087591bd84560150584cd7f58bf9
aren227 <mathdong01@gmail.com>
2021-03-26 19:21:03 +0900

    Switcher, PageConfig 뼈대 구현

---
+++ Assets/Scripts/PageConfig.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class PageConfig {
+    [SerializeField]
+    public string condition;
+    
+    [SerializeField]
+    public bool hide;
+
+    [SerializeField]
+    public string position;
+
+    [SerializeField]
+    public Sprite sprite;
+
+    [SerializeField]
+    public BScriptExecutor triggerExecutor;
+
+    [SerializeField]
+    public BScriptExecutor autoExecutor;
+}
