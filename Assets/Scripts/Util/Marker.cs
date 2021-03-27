78c0679c5882d82bece699b968ae8c741ffdb4af
aren227 <mathdong01@gmail.com>
2021-03-28 01:45:21 +0900

    마커 스크립트가 Gizmo를 그리지 않도록 변경

--- Assets/Scripts/Util/Marker.cs
+++ Assets/Scripts/Util/Marker.cs
@@ -8,18 +8,5 @@ public class Marker : MonoBehaviour
     public Face face = Face.UNKNOWN;
     public Vector2 position => transform.position;
 
-    public void OnDrawGizmos() {
-        Handles.color = Color.red;
-
-        GUIStyle style = new GUIStyle();
-        style.alignment = TextAnchor.MiddleCenter;
-        style.fontSize = 16;
-
-        Handles.Label(transform.position + new Vector3(0f, 0.7f, 0f), name, style);
-
-        Gizmos.color = Color.red;
-        Gizmos.DrawSphere(transform.position, 0.2f);
-    }
-
     public static implicit operator Vector2Face(Marker marker) => new Vector2Face(marker.position, marker.face);
 }
