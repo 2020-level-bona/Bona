084d6c18ae14d5b85923ae6d49d9a242b45e33ff
aren227 <mathdong01@gmail.com>
2021-03-28 22:44:16 +0900

    AnimationPalette에 조건식 추가, 복수 컴포넌트 지원

--- Assets/Scripts/Animation/AnimationPaletteEditor.cs
+++ Assets/Scripts/Animation/AnimationPaletteEditor.cs
@@ -6,39 +6,48 @@ using UnityEditor;
 [CustomEditor(typeof(AnimationPalette))]
 public class AnimationPaletteEditor : Editor
 {
+    bool isFolded = true;
+
     public override void OnInspectorGUI()
     {
         AnimationPalette palette = (AnimationPalette) target;
 
         var states = palette.GetAllStates();
-
-        EditorGUILayout.LabelField($"총 {states.Count}개의 State가 로드되었습니다.");
         
-        List<string> conflicts = palette.CheckConflicts();
-        if (conflicts.Count > 0) {
-            EditorGUILayout.LabelField($"{conflicts.Count}개의 이름 충돌이 발견되었습니다!", GetErrorTextStyle());
-            EditorGUILayout.LabelField(string.Join(", ", conflicts), GetErrorTextStyle());
-            EditorGUILayout.LabelField("부여된 별칭이 모두 고유하도록 재설정해주세요.", GetErrorTextStyle());
-        }
+        string prevCondition = palette.condition;
+        palette.condition = EditorGUILayout.TextField("조건식", serializedObject.FindProperty("condition").stringValue);
+        if (palette.condition != prevCondition)
+            EditorUtility.SetDirty(target);
 
-        foreach (string state in states) {
-            EditorGUILayout.BeginHorizontal();
-            EditorGUILayout.LabelField(state);
+        if (isFolded = EditorGUILayout.Foldout(isFolded, "States")) {
+            EditorGUILayout.LabelField($"총 {states.Count}개의 State가 로드되었습니다.");
 
-            string prev = palette.GetAlias(state);
-
-            string alias = EditorGUILayout.TextField(palette.GetAlias(state));
-            if (alias == null || alias.Length == 0) {
-                if (palette.GetAlias(state) != null)
-                    palette.RemoveAlias(state);
-            } else {
-                palette.SetAlias(state, alias);
+            List<string> conflicts = palette.CheckConflicts();
+            if (conflicts.Count > 0) {
+                EditorGUILayout.LabelField($"{conflicts.Count}개의 이름 충돌이 발견되었습니다!", GetErrorTextStyle());
+                EditorGUILayout.LabelField(string.Join(", ", conflicts), GetErrorTextStyle());
+                EditorGUILayout.LabelField("부여된 별칭이 모두 고유하도록 재설정해주세요.", GetErrorTextStyle());
             }
 
-            if (prev != palette.GetAlias(state))
-                EditorUtility.SetDirty(target);
+            foreach (string state in states) {
+                EditorGUILayout.BeginHorizontal();
+                EditorGUILayout.LabelField(state);
 
-            EditorGUILayout.EndHorizontal();
+                string prev = palette.GetAlias(state);
+
+                string alias = EditorGUILayout.TextField(palette.GetAlias(state));
+                if (alias == null || alias.Length == 0) {
+                    if (palette.GetAlias(state) != null)
+                        palette.RemoveAlias(state);
+                } else {
+                    palette.SetAlias(state, alias);
+                }
+
+                if (prev != palette.GetAlias(state))
+                    EditorUtility.SetDirty(target);
+
+                EditorGUILayout.EndHorizontal();
+            }
         }
     }
 
